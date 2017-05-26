b0df18d67daba01f77a1920cf31d909ba5095321
gearl <365789624@qq.com>
2017-05-26 17:55:13 +0800

    移除自动化脚本setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-pip install virtualenv
-virtualenv env
-source ./env/bin/activate
-pip install -r requirements.txt
-cp .env.sample .env
-nano .env
-honcho run python manage.py syncdb
-honcho run python manage.py runserver --port 8080
