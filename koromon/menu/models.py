02c162d85af4c743fbe317c97ad84b9fca7bb4a6
gearl <365789624@qq.com>
2017-06-09 17:33:40 +0800

    为菜单增加跳转方式选项

--- koromon/menu/models.py
+++ koromon/menu/models.py
@@ -9,12 +9,14 @@ from koromon.exts.database import db
 
 class Menu(Base):
     __tablename__ = 'menu'
+    MENU_JUMP_MODE_VALUE = ('_blank', '_self')
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(16))
     type = db.Column(db.Integer, default=1)  # 默认为1，1为一级目录，2为二级目录，不支持更高级目录
     sort = db.Column(db.Integer, default=0)
     parent_id = db.Column(db.Integer, default=0)  # 区分二级目录归在哪个一级目录下
     url = db.Column(db.String(512))
+    jump_mode = db.Column(db.Enum(*MENU_JUMP_MODE_VALUE), default='_self')
     created = db.Column(db.DateTime, default=datetime.utcnow)
     deleted = db.Column(db.BOOLEAN, default=False)
 
